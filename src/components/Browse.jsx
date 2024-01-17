bfff285adb3c1c4fa462581aa9b632d826dad668
Santhossh15 <santhossh15@gmail.com>
2024-01-17 12:04:41 +0530

    FRAMED BACKGROUND VIDEO IN HOME PAGE && CUSTOM HOOKS

--- src/components/Browse.jsx
+++ src/components/Browse.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import Header from "./Header";
 import useNowPlayingMovies from "../hooks/UseNowPlayingMovies";
+import MainContainer from "./MainContainer";
+import SecondaryContainer from "./SecondaryContainer";
 
 const Browse = () => {
   //Fetch Data from TMDB API and update the stored movies
@@ -9,6 +11,8 @@ const Browse = () => {
   return (
     <div>
       <Header />
+      <MainContainer />
+      <SecondaryContainer />
     </div>
   );
 };
--- src/hooks/UseNowPlayingMovies.jsx
+++ src/hooks/UseNowPlayingMovies.jsx
@@ -8,11 +8,10 @@ const useNowPlayingMovies = () => {
 
   const getNowPlayingMovies = async () => {
     const data = await fetch(
-      "https://api.themoviedb.org/3/movie/now_playing?language=en-US&page=1",
+      "https://api.themoviedb.org/3/movie/now_playing?&page=1",
       API_OPTIONS
     );
     const json = await data.json();
-    console.log(json.results);
     dispatch(addNowPlayingMovies(json.results));
   };
 
